e94cae69098d9e79d518a82a0b1bb8a67ac3a9c2
Jean Dunkl <63207852+JeanDunkl@users.noreply.github.com>
2020-04-08 14:59:19 -0300

    inicio aula

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+var nome = "Jean Dunkl";
+var idade = 42;
+var frase = "Papai é FODA"
+//alert(nome + " tem " + idade + " anos ");
+
+console.log(idade)
+console.log(nome)
+console.log(frase)
+alert(frase)
